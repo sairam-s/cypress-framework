832476d22e88fff47069033fe1cf296031fefd7c
Sairam Sivaswami <ss_sairam@yahoo.com>
2019-10-16 08:36:05 +1300

    navigation test

---
+++ cypress/integration/examples/navigation.spec.js
@@ -0,0 +1,13 @@
+/// <reference types="Cypress" />
+
+describe("Sample test for navigation", () => {
+
+    it("test ", () => {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
+        cy.get('a > .btn').click();
+        cy.get("li").contains("Practice").click();
+        cy.go('back');
+        cy.get('.container').find("h2").eq(0).should('have.text', 'Featured Courses');
+    })
+
+})
